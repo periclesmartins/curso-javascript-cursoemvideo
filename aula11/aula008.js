06619e010537ba11773a37667c6797694b63a974
Péricles <perespericles9@gmail.com>
2023-01-17 15:40:36 -0300

    Exercícios de condições em js, if e else.

---
+++ aula11/aula008.js
@@ -0,0 +1,12 @@
+// Condições usando if e else.
+
+let vel = 70
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!!!`)
+}
+else {
+    console.log(`A velocidade do seu carro é ${vel}Km/h, tudo ok!`)
+    console.log(`Dirija sempre usando cinto de segurança!`)
+}
+
