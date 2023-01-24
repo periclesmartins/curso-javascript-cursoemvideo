25455b0bd80e5e624703cab05b2d9489e02e7c38
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-01-24 00:28:57 -0300

    Aprendendo 'while' e 'do while'

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+//aprendendo repetições.
+
+let c = 1
+do { 
+    console.log(`Passo ${c}`)
+    c++ 
+} while (c <= 6)
+
+/*
+let c = 1
+while (c <= 10) { //vai repetir 6 vezes o console.log(`Passo ${c}`)
+    console.log(`Passo ${c}`)
+    c++ //é o mesmo que; c = c + 1
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
