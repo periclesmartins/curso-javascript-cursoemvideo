f82a3d21ab941c0aaaaf578eb9e5e89148fd4232
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-01-25 16:50:48 -0300

    Modifiquei a tabuada.
    Adicionei a função de usar a tecla Enter para pesquisar.
    Coloquei um áudio engraçado para descontrair.
    Mudei o estilo da tabuada.

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -16,6 +16,11 @@ function tabuada() {
         }
     }
 }
+document.getElementById("txtn").addEventListener("keyup", function(event) {
+    if (event.code === 'Enter') {
+        tabuada();
+    }
+});
 
 /*
 Esse código é uma função JavaScript que calcula a tabuada de um número específico. A função pega o valor digitado pelo usuário em um elemento de input com o ID "txtn", verifica se o valor está vazio e, se estiver, exibe uma mensagem de alerta para o usuário digitar um número. Caso contrário, a função pega o valor digitado, converte-o para um número (Number) e usa um loop "while" para calcular e exibir cada produto da tabuada (de 1 a 10) em um elemento "select" com o ID "seltab".
