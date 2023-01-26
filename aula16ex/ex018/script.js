11d1666edd263ad77a890ff2fcb7d21bf701bb51
Péricles <115372125+periclesmartins@users.noreply.github.com>
2023-01-26 17:16:56 -0300

    coloquei a função inalizar

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -21,9 +21,18 @@ function inLista(n, l) {
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
     } else {
         alert("Valor inválido ou já encontrado na lista.")
     }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    
 }
 
